f2a62f62441039744d0e3c35be5c2e58a0f994c7
Analuvazz <analuvaz1997@gmail.com>
2024-07-01 22:20:48 -0300

    subir codigo

---
+++ prog_meses.js
@@ -0,0 +1,9 @@
+const meses =['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho','Julho','Agosto','Setembro','Outubro'];
+
+for (let i =0; i < meses.length; i++) {
+    if (meses [i] === 'Outubro'){
+        console.log("Nasci em Outubro");
+    } else{
+        console.log(meses[i]);
+    }
+}
---
+++ prog_meses.py
@@ -0,0 +1,8 @@
+meses = [ 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho','Julho', 'Agosto','Setembro','Outubro']
+
+for mes in meses: 
+    if mes == 'Outubro':
+        print(f"Nasci em {mes}")
+
+    else:
+        print(mes)
